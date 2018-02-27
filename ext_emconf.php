ad9bf0d55ec3379e6600c62b11ebc989ef8fdf76
Nicole Cordes <typo3@cordes.co>
2018-02-27 23:53:16 +0100

    [TASK] Add basic extension files

---
+++ ext_emconf.php
@@ -0,0 +1,40 @@
+<?php
+
+/***************************************************************
+ * Extension Manager/Repository config file for ext "filefill".
+ *
+ * Auto generated 27-02-2018 23:52
+ *
+ * Manual updates:
+ * Only the data in the array - everything else is removed by next
+ * writing. "version" and "dependencies" must not be touched!
+ ***************************************************************/
+
+$EM_CONF[$_EXTKEY] = array (
+  'title' => 'File Fill',
+  'description' => 'Find and fetch missing local files from different remotes',
+  'category' => 'misc',
+  'author' => 'Nicole Cordes',
+  'author_email' => 'typo3@cordes.co',
+  'author_company' => 'biz-design',
+  'state' => 'stable',
+  'uploadfolder' => 0,
+  'createDirs' => '',
+  'clearCacheOnLoad' => 0,
+  'version' => '1.0.0',
+  'constraints' => 
+  array (
+    'depends' => 
+    array (
+      'typo3' => '7.6.0-8.7.99',
+    ),
+    'conflicts' => 
+    array (
+    ),
+    'suggests' => 
+    array (
+    ),
+  ),
+  '_md5_values_when_last_written' => 'a:2:{s:13:"composer.json";s:4:"39ab";s:12:"ext_icon.png";s:4:"970b";}',
+);
+

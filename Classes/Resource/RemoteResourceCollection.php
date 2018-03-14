76a7b836d21084481187f0f225bc5e4024105759
Nicole Cordes <typo3@cordes.co>
2018-03-15 00:14:19 +0100

    [TASK] Add header and license information

--- Classes/Resource/RemoteResourceCollection.php
+++ Classes/Resource/RemoteResourceCollection.php
@@ -1,6 +1,19 @@
 <?php
 namespace IchHabRecht\Filefill\Resource;
 
+/*
+ * This file is part of the TYPO3 extension filefill.
+ *
+ * (c) Nicole Cordes <typo3@cordes.co>
+ *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE file that was distributed with this source code.
+ */
+
 use TYPO3\CMS\Core\Utility\GeneralUtility;
 
 class RemoteResourceCollection
--- Classes/Slot/ResourceFactorySlot.php
+++ Classes/Slot/ResourceFactorySlot.php
@@ -1,6 +1,19 @@
 <?php
 namespace IchHabRecht\Filefill\Slot;
 
+/*
+ * This file is part of the TYPO3 extension filefill.
+ *
+ * (c) Nicole Cordes <typo3@cordes.co>
+ *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE file that was distributed with this source code.
+ */
+
 use IchHabRecht\Filefill\Resource\Driver\FileFillDriver;
 use IchHabRecht\Filefill\Resource\RemoteResourceCollectionFactory;
 use TYPO3\CMS\Core\Resource\Exception\InvalidConfigurationException;

7c6cb1e5645fb3132016b2398c6595cd4e796e1e
Nicole Cordes <typo3@cordes.co>
2018-03-15 22:11:32 +0100

    [FEATURE] Add slot to fetch processed files
    This also fetches the original file if missing.

---
+++ Classes/Slot/FileProcessingServiceSlot.php
@@ -0,0 +1,29 @@
+<?php
+namespace IchHabRecht\Filefill\Slot;
+
+/*
+ * This file is part of the TYPO3 extension filefill.
+ *
+ * (c) Nicole Cordes <typo3@cordes.co>
+ *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE file that was distributed with this source code.
+ */
+
+use TYPO3\CMS\Core\Resource\Driver\DriverInterface;
+use TYPO3\CMS\Core\Resource\File;
+use TYPO3\CMS\Core\Resource\ProcessedFile;
+use TYPO3\CMS\Core\Resource\Service\FileProcessingService;
+
+class FileProcessingServiceSlot
+{
+    public function ensureOriginalFileExists(FileProcessingService $fileProcessingService, DriverInterface $driver, ProcessedFile $processedFile, File $file)
+    {
+        // Call exists() function to ensure driver fetches missing files
+        $processedFile->exists() && $file->exists();
+    }
+}
--- ext_localconf.php
+++ ext_localconf.php
@@ -1,12 +1,21 @@
 <?php
 defined('TYPO3_MODE') || die();
 
-$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['processDatamapClass']['filefill'] =
-    \IchHabRecht\Filefill\Hooks\ResetMissingFiles::class;
+call_user_func(function () {
+    $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['processDatamapClass']['filefill'] =
+        \IchHabRecht\Filefill\Hooks\ResetMissingFiles::class;
 
-\TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(TYPO3\CMS\Extbase\SignalSlot\Dispatcher::class)->connect(
-    \TYPO3\CMS\Core\Resource\ResourceFactory::class,
-    \TYPO3\CMS\Core\Resource\ResourceFactoryInterface::SIGNAL_PostProcessStorage,
-    \IchHabRecht\Filefill\Slot\ResourceFactorySlot::class,
-    'initializeResourceStorage'
-);
+    $dispatcher = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(TYPO3\CMS\Extbase\SignalSlot\Dispatcher::class);
+    $dispatcher->connect(
+        \TYPO3\CMS\Core\Resource\ResourceFactory::class,
+        \TYPO3\CMS\Core\Resource\ResourceFactoryInterface::SIGNAL_PostProcessStorage,
+        \IchHabRecht\Filefill\Slot\ResourceFactorySlot::class,
+        'initializeResourceStorage'
+    );
+    $dispatcher->connect(
+        \TYPO3\CMS\Core\Resource\ResourceStorage::class,
+        \TYPO3\CMS\Core\Resource\Service\FileProcessingService::SIGNAL_PreFileProcess,
+        \IchHabRecht\Filefill\Slot\FileProcessingServiceSlot::class,
+        'ensureOriginalFileExists'
+    );
+});

3eb543c6556967cffb19284ec84a19a4ad5b7be4
Nicole Cordes <typo3@cordes.co>
2020-03-22 16:16:35 +0100

    [FEATURE] Add functional tests

---
+++ Tests/Functional/AbstractFunctionalTestCase.php
@@ -0,0 +1,54 @@
+<?php
+namespace IchHabRecht\Filefill\Tests\Functional;
+
+/*
+ * This file is part of the TYPO3 extension filefill.
+ *
+ * (c) Nicole Cordes <typo3@cordes.co>
+ *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE file that was distributed with this source code.
+ */
+
+use Nimut\TestingFramework\TestCase\FunctionalTestCase;
+
+class AbstractFunctionalTestCase extends FunctionalTestCase
+{
+    protected $additionalFoldersToCreate = [
+        'wikipedia',
+    ];
+
+    protected $configurationToUseInTestInstance = [
+        'EXTCONF' => [
+            'filefill' => [
+                'storages' => [
+                    1 => [
+                        'domain' => [
+                            'https://upload.wikimedia.org',
+                        ],
+                        'placeholder' => true,
+                    ],
+                ],
+            ],
+        ],
+    ];
+
+    protected $testExtensionsToLoad = [
+        'typo3conf/ext/filefill',
+    ];
+
+    protected function setUp()
+    {
+        parent::setUp();
+
+        $fixturePath = ORIGINAL_ROOT . 'typo3conf/ext/filefill/Tests/Functional/Fixtures/Database/';
+        $this->importDataSet($fixturePath . 'sys_file_storage.xml');
+        $this->importDataSet($fixturePath . 'sys_file.xml');
+
+        $this->setUpBackendUserFromFixture(1);
+    }
+}

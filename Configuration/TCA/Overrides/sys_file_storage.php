6a32307f75527f0bc0381a6318179ee9618df3b8
Thomas Löffler <loeffler@spooner-web.de>
2022-03-15 22:12:57 +0100

    Change TYPO3_MODE to TYPO3

--- Configuration/TCA/Overrides/sys_file_storage.php
+++ Configuration/TCA/Overrides/sys_file_storage.php
@@ -15,7 +15,7 @@ declare(strict_types=1);
  * LICENSE file that was distributed with this source code.
  */
 
-defined('TYPO3_MODE') || die();
+defined('TYPO3') || die();
 
 $tempColumns = [
     'tx_filefill_enable' => [

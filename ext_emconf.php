bcde45d187eeb17ed0895f3b37d54ff828fb70d8
Elias Häußler <e.haeussler@familie-redlich.de>
2023-04-03 14:34:14 +0200

    [TASK] Add support for TYPO3 v12.3

--- ext_emconf.php
+++ ext_emconf.php
@@ -26,7 +26,7 @@ $EM_CONF[$_EXTKEY] = array (
   array (
     'depends' =>
     array (
-      'typo3' => '10.4.11-12.2.99',
+      'typo3' => '10.4.11-12.3.99',
     ),
     'conflicts' =>
     array (

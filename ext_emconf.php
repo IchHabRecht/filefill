d070c2367bcd1c79decda3209c0560f28713fbc5
Nicole Cordes <typo3@cordes.co>
2024-02-08 16:56:29 +0100

    [RELEASE] Release of filefill 4.3.1

--- ext_emconf.php
+++ ext_emconf.php
@@ -21,7 +21,7 @@ $EM_CONF[$_EXTKEY] = array (
   'uploadfolder' => 0,
   'createDirs' => '',
   'clearCacheOnLoad' => 0,
-  'version' => '4.3.0',
+  'version' => '4.3.1',
   'constraints' =>
   array (
     'depends' =>

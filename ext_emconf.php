80a3dec56e5d6108b26dfdb940311631052a8653
Nicole Cordes <typo3@cordes.co>
2024-12-18 00:27:39 +0100

    [FEATURE] Raise compatibility to TYPO3 13.4

--- ext_emconf.php
+++ ext_emconf.php
@@ -26,7 +26,7 @@ $EM_CONF[$_EXTKEY] = array (
   array (
     'depends' =>
     array (
-      'typo3' => '10.4.11-12.4.99',
+      'typo3' => '11.5.0-13.4.99',
     ),
     'conflicts' =>
     array (

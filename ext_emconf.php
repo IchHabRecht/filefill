ccd4aa97b88d96c78404fec99307ea51f1a4503e
Nicole Hummel <nicole-typo3@nimut.dev>
2025-04-04 10:12:56 +0200

    [TASK] Raise extension version to 5.x

--- ext_emconf.php
+++ ext_emconf.php
@@ -21,7 +21,7 @@ $EM_CONF[$_EXTKEY] = array (
   'uploadfolder' => 0,
   'createDirs' => '',
   'clearCacheOnLoad' => 0,
-  'version' => '4.4.0',
+  'version' => '5.0.0',
   'constraints' =>
   array (
     'depends' =>
